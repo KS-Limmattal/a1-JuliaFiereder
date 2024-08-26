2e52d8481a338249ac654380a1b4fc0556f6f530
Your Name <you@example.com>
2024-08-26 15:31:38 +0200

    abgabe

--- Methoden.java
+++ Methoden.java
@@ -1,16 +1,39 @@
  
 public class Methoden {
 
-    public static int round(int zahl){
+    //public static int round(int zahl){
         // TODO: implementiere hier deine Lösung für a)
-        return 0; // diese Zeile darf verändert werden :-)
-    }
+         // diese Zeile darf verändert werden :-)
+    //}
 
     public static void hours(int seconds){
         // TODO: implementiere hier deine Lösung für b)
+        int hours = seconds / 3600;
+        int minutes = seconds % 3600 / 60;
+        seconds = seconds % 60;
+        System.out.println(hours + ":" + minutes + ":" + seconds);
     }
     
     // TODO: Schreibe hier eine Methode für die Aufgabe c)
+    public static void sort(int a, int b ,int c){
+        if (a > b){
+            int glub = a;
+            a = b;
+            b = glub;
+        }
+        if (a > c){
+            int glub = a;
+            a = c;
+            c = glub;
+        }
+        if (b > c){
+            int glub = b;
+            b = c;
+            c = glub;
+        }
+    }
+    
+
     
     public static int distance(double x1, double y1, double x2, double y2){
         // TODO: implementiere hier deine Lösung für d)
@@ -19,17 +42,17 @@ public class Methoden {
 
         public static void main(String[] args){
         // Test-Code für Teilaufgabe a)
-        System.out.println("Aufgabe a)");
-        System.out.println(round(149));
-        System.out.println(round(150));
-        System.out.println(round(-50));
-        System.out.println(round(-49));
-        System.out.println();
+        //System.out.println("Aufgabe a)");
+        //System.out.println(round(149));
+        //System.out.println(round(150));
+        //System.out.println(round(-50));
+        //System.out.println(round(-49));
+        //System.out.println();
 
         // Test-Code für Teilaufgabe b)
         System.out.println("Aufgabe b)");
         // TODO: Schreibe Testcode für die Aufgabe
-        System.out.println();
+        hours(1234);
 
         // Test-Code für Teilaufgabe c)
         System.out.println("Aufgabe c)");
